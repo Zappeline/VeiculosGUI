f6decd0ac12079832bed73f124f75c6d0c4b106e
Zappeline <zappeline1999@gmail.com>
2025-06-02 21:39:54 -0300

    02/06/2025

--- models/Proprietario.py
+++ models/Proprietario.py
@@ -1,12 +1,9 @@
-from .Veiculo import Veiculo
-
-
-class Proprietario(Veiculo):
+class Proprietario:
     def __init__(self, nome, cpf, placa_veiculo=None):
         self.nome = nome
         self.cpf = cpf
         self.placa_veiculo = placa_veiculo # Placa do veículo que possui
-        super().__init__(self, modelo=None)
+
        
 
     def get_nome(self):
@@ -20,6 +17,6 @@ class Proprietario(Veiculo):
 
 
     def __str__(self):
-        if f"({self.placa_veiculo})":
-            return (f"Nome: {self.nome}, CPF: {self.cpf}, Modelo: {self.__modelo} Placa: {self.placa_veiculo}")
-        else: "Nenhum"
+        f"({self.placa_veiculo})" if self.placa_veiculo else "Nenhum"
+        return (f"Nome: {self.nome}, CPF: {self.cpf}, Placa: {self.placa_veiculo}")
+       
